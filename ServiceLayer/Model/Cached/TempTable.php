967d860c50e8387332b446bfe4b664e17ce8f2e8
Laticinios PJ <Laticinios PJ@192.168.1.51>
2014-07-04 15:51:39 -0300

    Update 04/07

---
+++ ServiceLayer/Model/Cached/TempTable.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Model\Cached;
+
+/**
+ * Description of TempTable
+ *
+ * @author Luis Paulo
+ */
+class TempTable {
+
+    public $conn, $values, $ponter = 0;
+    private $_it;
+
+    public function __construct() {
+        $this->conn = \Model\Connection\Init::getInstance()->on();
+        if (!$this->createTable()) {
+            throw new \Exceptions\ClientExceptionResponse("Temp table was not created!");
+        }
+        $stmt = $this->conn->query("SELECT * FROM tmpReport");
+        $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        $this->_it = new \ArrayIterator($stmt->fetchAll(\PDO::FETCH_ASSOC));
+    }
+
+    private function createTable() {
+        return $this->conn->exec($this->sql);
+    }
+
+    public function reload($id = false) {
+        $sql = $id ? sprintf(" WHERE id=%u", $id) : "";
+        $stmt = $this->conn->query("SELECT * FROM tmpReport$sql");
+        $this->_it = null;
+        return $this->_it = new \ArrayIterator($stmt->fetchAll(\PDO::FETCH_ASSOC));
+    }
+
+    public function count() {
+        return $this->_it->count();
+    }
+
+    public function save(&$stmt = null) {
+        $stmt = $this->conn->prepare("INSERT INTO tmpReport (id,produtor,saldo,impureza_media,"
+                . "service_secagem,quebra_peso, umidade_media, quebra_tecnica,service_armazenagem)"
+                . "VALUES(:id,:p,:s,:i,:g,:qp,:u, :qt,:a)");
+        $stmt->bindValue(':id', $this->id);
+        $stmt->bindValue(':p', $this->produtor, \PDO::PARAM_STR);
+        $stmt->bindValue(':s', $this->saldo);
+        $stmt->bindValue(':i', $this->impureza_media);
+        $stmt->bindValue(':u', $this->umidade_media);
+        $stmt->bindValue(':g', $this->service_secagem);
+        $stmt->bindValue(':qp', $this->quebra_peso);
+        $stmt->bindValue(':qt', $this->quebra_tecnica);
+        $stmt->bindValue(':a', $this->service_armazenagem);
+        if ($stmt->execute()) {
+            $this->values = [];
+
+            return true;
+        }
+        return false;
+    }
+
+    public static function drop() {
+        return \Model\Connection\Init::getInstance()->on()->exec("DROP TABLE tmpReport;");
+    }
+
+    public function __get($name) {
+        if ($this->_it->offsetExists($this->ponter)) {
+            return $this->_it->offsetGet($this->ponter)[$name];
+        } elseif (isset($this->values[$name])) {
+            return $this->values[$name];
+        } else {
+            return null;
+        }
+    }
+
+    public function __set($name, $value) {
+        if ($this->_it->offsetExists($this->ponter))
+            $this->_it->offsetGet($this->ponter)[$name] = $value;
+        else
+            $this->values[$name] = $value;
+    }
+
+    public function __toString() {
+        return json_encode($this->_it->getArrayCopy());
+    }
+
+    public $sql = <<<SQL
+CREATE TEMPORARY TABLE IF NOT EXISTS tmpReport(
+    id INTEGER NOT NULL ,
+    produtor TEXT NOT NULL ,
+    saldo REAL NOT NULL DEFAULT 0.00,
+    impureza_media REAL NOT NULL DEFAULT 0.00,
+    umidade_media REAL NOT NULL DEFAULT 0.00,
+    service_secagem REAL NOT NULL DEFAULT 0.00,
+    quebra_peso REAL NOT NULL DEFAULT 0.00,
+
+    
+    quebra_tecnica REAL NOT NULL DEFAULT 0.00,
+    service_armazenagem REAL NOT NULL DEFAULT 0.00
+);
+SQL;
+
+}
---
+++ tests/ServiceLayer/Model/Cached/TempTableTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use \PHPUnit_Framework_TestCase as PHPUnit;
+
+require_once dirname(dirname(__DIR__)) . '/DBConnSqliteTest.php';
+
+/**
+ * Description of TempTableTest
+ *
+ * @author Luis Paulo
+ */
+class TempTableTest extends PHPUnit {
+
+    private $tmpt;
+
+    protected function setUp() {
+        DBConnSqliteTest::ConnPDO();
+        $this->tmpt = new \Model\Cached\TempTable();
+    }
+
+    protected function tearDown() {
+        \Model\Cached\TempTable::drop();
+    }
+
+    public function testPropertiesSetAndGet() {
+        $tmpt = $this->tmpt;
+        $tmpt->id = 1;
+        $tmpt->produtor = "Luis Paulo";
+        $tmpt->saldo = 97450;
+        $tmpt->impureza_media = 1.56;
+        $tmpt->umidade_media = 13;
+        $tmpt->service_secagem = 15000;
+        $tmpt->quebra_peso = 7540;
+        $tmpt->quebra_tecnica = 542;
+        $tmpt->service_armazenagem = 2500;
+
+        $this->assertEquals($tmpt->count(), 0);
+        $this->assertTrue($tmpt->save($stmt));
+        $this->assertEquals($stmt->getSQL(), $this->expected);
+
+        $tmpt->reload();
+        $this->assertEquals($tmpt->count(), 1);
+        $this->assertEquals($tmpt->saldo, '97450');
+        echo $tmpt;
+    }
+
+//    /**
+//     * @expectedException  Exceptions\ClientExceptionResponse
+//     */
+//    public function testExceptionTmpTableHasBeCreated() {
+//        $reflection = new ReflectionClass('\Model\Cached\TempTable');
+//        $method = $reflection->getMethod('createTable');
+//        $method->setAccessible(true);
+//        $method->invoke($this->tmpt);
+//    }
+
+    private $expected = "INSERT INTO tmpReport (id,produtor,saldo,impureza_media,service_secagem,quebra_peso, umidade_media, quebra_tecnica,service_armazenagem)VALUES('1','Luis Paulo','97450','1.56','15000','7540','13', '542','2500')";
+
+}
